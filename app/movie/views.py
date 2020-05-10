cc9f533f50154011212b9a70aa6ac5b35f863ac1
Arsener <1769879278@qq.com>
2020-05-10 10:52:14 +0800

    referer

--- app/movie/views.py
+++ app/movie/views.py
@@ -1,6 +1,7 @@
 from . import movie
-from flask import request, redirect, render_template, session, url_for
+from flask import request, redirect, render_template, session, url_for, Response
 from ..db import get_db
+import requests
 
 @movie.route('/', methods=['GET', 'POST'])
 def index():
@@ -11,9 +12,9 @@ def index():
         db = get_db()
         cur = db.cursor()
         sql = '''
-            select movie_name_cn from movie limit %s
+            select movie_id, movie_name_cn, poster_url from movie where movie_name_cn like %s
         '''
-        cur.execute(sql, (int(movie_name), ))
+        cur.execute(sql, ('%{}%'.format(movie_name), ))
         movie_list = cur.fetchall()
         # print(movie_list)
         session['hh'] = movie_list
@@ -23,3 +24,18 @@ def index():
         return redirect(url_for('movie.index'))
 
     return render_template('movie_list.html', movie_name=session.get('movie_name'), l=session.get('hh'))
+
+
+@movie.route('/image/', methods=['GET'])
+def image():
+    image_url = request.args.get('image_url')
+    conn = requests.get(
+        url=image_url,
+        headers={
+            "Origin": "https://www.douban.com",
+            "Referer": "https://www.douban.com/",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        }
+    )
+
+    return Response(conn.content, mimetype="image/jpeg")
